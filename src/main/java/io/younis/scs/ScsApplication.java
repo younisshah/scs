a2006fa2bdbf705f3d5d3f7744bdfe5f346b9822
Younis Shah <younisashah@gmail.com>
2020-03-27 19:44:16 +0700

    feat: initial commit

---
+++ src/main/java/io/younis/scs/ScsApplication.java
@@ -0,0 +1,13 @@
+package io.younis.scs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScsApplication.class, args);
+    }
+
+}
